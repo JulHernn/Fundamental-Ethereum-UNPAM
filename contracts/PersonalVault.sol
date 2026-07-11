d6110b9cd2cec2a6e446976a17d7138b2171e111
Julian Hernan <julianhernan07@gmail.com>
2026-07-11 23:54:15 +0700

    feat: implement complete system

--- contracts/PersonalVault.sol
+++ contracts/PersonalVault.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.20;
 
 /// @title Time-Locked Personal Vault
-/// @notice TODO: implement deposit(), withdraw(), and extendLock() sesuai spesifikasi di README.md
 contract PersonalVault {
     address public owner;
     uint256 public unlockTime;
@@ -14,27 +13,44 @@ contract PersonalVault {
     error FundsLocked();
     error NotOwner();
     error InvalidUnlockTime();
+	error NoBalance();
+	error TransferFailed();
 
     constructor(uint256 _unlockTime) {
-        // TODO: validate _unlockTime tidak di masa lalu
+        if (_unlockTime < block.timestamp)
+			revert InvalidUnlockTime();
         owner = msg.sender;
         unlockTime = _unlockTime;
     }
 
     modifier onlyOwner() {
-        // TODO
+        if (msg.sender != owner)
+			revert NotOwner();
         _;
     }
 
     function deposit() external payable {
-        // TODO
+		emit Deposit(msg.sender, msg.value);
     }
 
     function withdraw() external onlyOwner {
-        // TODO
+        if (address(this).balance == 0)
+            revert NoBalance();
+        uint256 currentTime = block.timestamp;
+        if (currentTime < unlockTime)
+            revert FundsLocked();
+        uint256 withdrawAmount = address(this).balance;
+        (bool success, ) = owner.call{value: withdrawAmount}("");
+        if (!success)
+            revert TransferFailed();
+		emit Withdrawal(owner, withdrawAmount);
     }
 
     function extendLock(uint256 newTime) external onlyOwner {
-        // TODO
+        if (newTime <= unlockTime)
+			revert InvalidUnlockTime();
+		uint256 previousUnlockTime = unlockTime;
+		unlockTime = newTime;
+		emit LockExtended(previousUnlockTime, unlockTime);
     }
 }
